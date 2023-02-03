be5a5bf55392002cf5c3022c2c424ce20f267c6e
antwandesign <39638303+antwandesign@users.noreply.github.com>
2023-02-03 11:47:45 +0200

    Style changes

--- src/views/Tour/TourView.vue
+++ src/views/Tour/TourView.vue
@@ -1,3 +1,14 @@
 <template>
-    This is tour view
+    <div v-html="store.tour?.description"></div>
+    <ul v-if="store.tour">
+      <li v-for="floor in store.tour.structure" class="link">
+        <RouterLink class="link" :to="{ name: 'floor', params: { floorId: floor.id }, query: { key: 'IgSjubq3amMafYhP6EL18KAexCqTnZWQ' } }">{{ floor.name }}</RouterLink>
+      </li>
+    </ul>
 </template>
+<script setup lang="ts">
+import { useTourStore } from '@/stores/tourStore'
+const store = useTourStore()
+
+
+</script>
